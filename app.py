f762ec95cd393809e549ed0741339f74598ced3f
riccardo.deiuri <riccardo.deiuri@samtrevano.ch>
2024-09-16 10:41:28 +0200

    added code for authentication

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request
 import random, json
-
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from models.model import db
 from models.model import *
-
-from flask_migrate import Migrate
+from routes.auth import auth as bp_auth
 
 app = Flask(__name__)
+
+app.register_blueprint(bp_auth, url_prefix='/auth')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask_hello_admin:Admin$00@localhost/flask_hello'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,6 +22,18 @@ def load_quotes():
         return json.load(f)
 
 quotes = load_quotes()
+
+# flask_login user loader block
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()    
+    return user
 
 @app.route('/testuser/')
 def test():
@@ -56,6 +70,13 @@ def update_user():
     new_email = values['email']
     User.update_user_email(username, new_email)
     return f'Utente {username} modificato con successo'
+
+@app.route('/deleteuser', methods=['POST'])
+def delete_user():
+    values = request.json
+    username = values['username']
+    User.delete_user_by_username(username)
+    return f'Utente {username} eliminato con successo'
 
 # Endpoint per ottenere una citazione casuale
 @app.route('/random-text', methods=['GET'])
